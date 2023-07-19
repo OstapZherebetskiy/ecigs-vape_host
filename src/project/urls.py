7306c01f7d6eb85f393cb61156e63a303b921049
Oleh <salitrunskyy@gmail.com>
2023-07-20 01:56:44 +0300

    test index page

--- src/project/urls.py
+++ src/project/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 
 
@@ -13,6 +14,8 @@ urlpatterns = i18n_patterns(
     re_path(r'^%s/' % settings.ADMIN_LOCATION_URL, admin.site.urls),
     re_path(r'^account/', include('account.urls')),
     re_path(r'^i18n/', include('django.conf.urls.i18n')),
+
+    re_path(r'^$', TemplateView.as_view(template_name='index.html'), name="index_page"),
     prefix_default_language=False
 )
 
