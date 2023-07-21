24492c7f4d65f04c571dc6979ce2047166eb4506
Oleh <salitrunskyy@gmail.com>
2023-07-21 13:09:11 +0300

    task_send_email_verification_code exception handling

--- src/apps/account/tasks.py
+++ src/apps/account/tasks.py
@@ -13,12 +13,15 @@ log = getLogger(__name__)
 def task_send_email_verification_code(user_id):
     user = User.objects.filter(pk=user_id).first()
     if user:
-        code = user.generate_new_verification_code(request_mail_verification=True)
-        subject, message, from_email, recipient_list = 'Email Verification', \
-                                                       f'Your verification code is {code}', \
-                                                       None, \
-                                                       [user.email]
-        send_mail(subject, message, from_email, recipient_list)
-        log.debug(f"Sent mail verification code to user {user}")
-        user.verification_code = code
-        user.save()
+        try:
+            code = user.generate_new_verification_code(request_mail_verification=True)
+            subject, message, from_email, recipient_list = 'Email Verification', \
+                                                        f'Your verification code is {code}', \
+                                                        None, \
+                                                        [user.email]
+            send_mail(subject, message, from_email, recipient_list)
+            log.debug(f"Sent mail verification code to user {user}")
+            user.verification_code = code
+            user.save()
+        except Exception as e:
+            log.error(e)
