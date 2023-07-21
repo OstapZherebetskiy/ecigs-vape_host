1c28215e2e28d9ae7458e62d29f8ff6cb13a08bf
Oleh <salitrunskyy@gmail.com>
2023-07-21 13:05:34 +0300

    task_send_email_verification_code fix

--- src/apps/account/tasks.py
+++ src/apps/account/tasks.py
@@ -14,10 +14,11 @@ def task_send_email_verification_code(user_id):
     user = User.objects.filter(pk=user_id).first()
     if user:
         code = user.generate_new_verification_code(request_mail_verification=True)
-        subject, message, recipient_list = 'Email Verification', \
-                                           f'Your verification code is {code}', \
-                                           [user.email]
-        send_mail(subject, message, recipient_list)
+        subject, message, from_email, recipient_list = 'Email Verification', \
+                                                       f'Your verification code is {code}', \
+                                                       None, \
+                                                       [user.email]
+        send_mail(subject, message, from_email, recipient_list)
         log.debug(f"Sent mail verification code to user {user}")
         user.verification_code = code
         user.save()
