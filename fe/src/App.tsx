cadbd7c427a35a00063e2e6752e022c3cbac099f
Taras Zherebetskiy <tarasj76@gmail.com>
2023-07-25 18:21:54 +0300

    mocked data, card grid

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,22 +1,10 @@
-import { useEffect } from 'react'
-import { fetchJson } from '@/common/fetchJson'
 import ErrorBoundary from '@/common-ui/errorBoundary'
-import { TextField } from './common-ui/text-field/TextField'
+import { ItemsGrid } from './components/items-grid'
 
 function App() {
-  useEffect(() => {
-    fetchData()
-  }, [])
-
-  const fetchData = async () => {
-    const data = await fetchJson('http://localhost:8000/api/goods/category/')
-    console.log(data)
-  }
-
   return (
     <ErrorBoundary>
-      <TextField />
-      <div>lalala</div>
+      <ItemsGrid />
     </ErrorBoundary>
   )
 }
---
+++ fe/src/components/items-grid/ItemsGrid.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import style from './ItemsGrid.module.scss'
+import { Product } from '@/api/product-microservice/types'
+import { productsApi } from '@/api/product-microservice'
+
+export const ItemsGrid = () => {
+  const [productList, setProductList] = useState<Product[]>([])
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  const fetchProducts = async () => {
+    const data = await productsApi.getProducts()
+
+    setProductList(data)
+
+    console.log(data)
+  }
+
+  return (
+    <ul className={style.container}>
+      {productList.map((product) => (
+        <li className={style.card}>
+          <h3 className={style.title}>{product.name}</h3>
+        </li>
+      ))}
+    </ul>
+  )
+}
