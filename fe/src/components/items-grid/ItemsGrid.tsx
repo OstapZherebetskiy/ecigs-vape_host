d05d8a83ba49cb135a8edf7b85d91f067679727d
Taras Zherebetskiy <tarasj76@gmail.com>
2023-07-27 21:41:31 +0300

    Card item

--- fe/src/components/items-grid/ItemsGrid.tsx
+++ fe/src/components/items-grid/ItemsGrid.tsx
@@ -16,22 +16,32 @@ export const ItemsGrid = () => {
     const data = await productsApi.getProducts()
 
     setProductList(data)
+  }
 
-    console.log(data)
+  const getFormatedDesc = (desc: string) => {
+    if (desc.length <= 130) return desc
+
+    const newDesc = desc.slice(0, 130).split(' ')
+
+    return newDesc.slice(0, newDesc.length).join(' ') + ' ...'
   }
 
   return (
     <ul className={style.container}>
       {productList.map((product) => (
         <li className={style.card}>
-          <h3 className={style.title}>{product.name}</h3>
-          <img className={style.img} src={product.main_photo} alt={product.name} />
+          <div className={style.box}>
+            <h3 className={style.title}>{product.name}</h3>
+            <div className={style.img__box}>
+              <img className={style.img} src={product.main_photo} alt={product.name} />
+            </div>
+          </div>
           {product.in_stock ? (
             <span className={style.present}>В наявності</span>
           ) : (
             <span className={style.absent}>Товар відсутній</span>
           )}
-          <p className={style.desc}>{product.description}</p>
+          <p className={style.desc}>{getFormatedDesc(product.description)}</p>
           <span className={style.price}>2000 грн</span>
           <Button className={style.btn}>Купити</Button>
         </li>
