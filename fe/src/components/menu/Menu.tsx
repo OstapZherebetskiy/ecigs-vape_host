114032437471e5f07ec69ff97b3f75c1ba9458e1
Nazarii Sova <madrigalamar@gmail.com>
2023-07-28 14:24:18 +0300

    add menu

--- fe/src/components/menu/Menu.tsx
+++ fe/src/components/menu/Menu.tsx
@@ -1,4 +1,24 @@
+import styles from '../menu/Menu.module.scss'
+import closeImg from '../img/close.png'
+import accountImg from '../img/account.png'
+import cartImg from '../img/cart.png'
 
-export const Menu = () => {
-
+export const Menu = ({closeMenu}) => {
+   
+    return (
+      <div className={styles.container}>
+        <div className={styles.account}>
+          <img className={styles.account_icon} src={accountImg} alt="account" />
+          <div className={styles.account_text}>Увійти</div>
+        </div>
+        <div className={styles.cart}>
+          <img className={styles.cart_icon} src={cartImg} alt="cart" />
+          <div className={styles.cart_text}>Кошик</div>
+        </div>
+        <div className={styles.close}>
+          <img className={styles.close_icon} onClick={closeMenu} src={closeImg} alt="close" />
+        </div>
+      </div>
+    )
+  
 }
