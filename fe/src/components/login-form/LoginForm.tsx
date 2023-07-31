0c5e47485c7909e4d90f9c42bcfd9233d65ffe1f
Taras Zherebetskiy <tarasj76@gmail.com>
2023-07-31 20:33:14 +0300

    fix text field

--- fe/src/components/login-form/LoginForm.tsx
+++ fe/src/components/login-form/LoginForm.tsx
@@ -1,6 +1,7 @@
 import { TextField } from '@/common-ui/text-field/TextField'
 import { ChangeEvent, FormEvent, useState } from 'react'
 import { Button } from '@/common-ui/button'
+import cn from 'classnames'
 
 import eye_close from '@/img/eye_close.png'
 import eye_open from '@/img/eye_open.png'
@@ -8,19 +9,26 @@ import eye_open from '@/img/eye_open.png'
 import style from './LoginForm.module.scss'
 
 enum InputType {
-  login = 'Login',
-  password = 'Password',
+  login = 'Логін',
+  passwordFirst = 'Пароль',
+  passwordSecond = 'Повтор паролю',
 }
 
 export const LoginForm = () => {
-  const [isShowPass, setIsShowPass] = useState(false)
-  const [values, setValues] = useState({
-    login: '',
-    password: '',
+  const [isNewUser, setIsNewUser] = useState(false)
+  const [isShowPass, setIsShowPass] = useState({
+    [InputType.passwordFirst]: false,
+    [InputType.passwordSecond]: false,
   })
 
-  const handleShowPass = () => {
-    setIsShowPass(!isShowPass)
+  const [values, setValues] = useState({
+    [InputType.login]: '',
+    [InputType.passwordFirst]: '',
+    [InputType.passwordSecond]: '',
+  })
+
+  const handleShowPass = (name: InputType.passwordFirst | InputType.passwordSecond) => {
+    setIsShowPass({ ...isShowPass, [name]: !isShowPass[name] })
   }
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -45,20 +53,60 @@ export const LoginForm = () => {
       />
       <div className={style.pass__box}>
         <TextField
-          type={isShowPass ? 'text' : 'password'}
-          placeholder={InputType.password}
+          type={isShowPass[InputType.passwordFirst] ? 'text' : 'password'}
+          placeholder={InputType.passwordFirst}
           onChange={handleChange}
-          name={InputType.password}
+          name={InputType.passwordFirst}
         />
         <img
           className={style.pass_btn}
-          onClick={handleShowPass}
-          src={isShowPass ? eye_close : eye_open}
-          alt={isShowPass ? 'show' : 'hide'}
+          onClick={() => handleShowPass(InputType.passwordFirst)}
+          src={isShowPass[InputType.passwordFirst] ? eye_close : eye_open}
+          alt={isShowPass[InputType.passwordFirst] ? 'show' : 'hide'}
         />
       </div>
 
-      <Button>Увійти</Button>
+      <div
+        className={cn(style.newUser__inputs, {
+          [style.active]: isNewUser,
+        })}
+      >
+        <div className={style.pass__box}>
+          <TextField
+            type={isShowPass[InputType.passwordSecond] ? 'text' : 'password'}
+            placeholder={InputType.passwordSecond}
+            onChange={handleChange}
+            name={InputType.passwordSecond}
+          />
+          <img
+            className={style.pass_btn}
+            onClick={() => handleShowPass(InputType.passwordSecond)}
+            src={isShowPass[InputType.passwordSecond] ? eye_close : eye_open}
+            alt={isShowPass[InputType.passwordSecond] ? 'show' : 'hide'}
+          />
+        </div>
+        <label className={style.newUser}>
+          <input
+            type="checkbox"
+            name="new_user"
+            checked={isNewUser}
+            onClick={() => setIsNewUser(!isNewUser)}
+          />
+          <span>Мені більше ніж 18 років</span>
+        </label>
+      </div>
+
+      <label className={style.newUser}>
+        <input
+          type="checkbox"
+          name="new_user"
+          checked={isNewUser}
+          onClick={() => setIsNewUser(!isNewUser)}
+        />
+        <span>У мене немає акаунта</span>
+      </label>
+
+      <Button type="submit">{isNewUser ? 'Зареєструвати аккаунт' : 'Увійти'}</Button>
     </form>
   )
 }
