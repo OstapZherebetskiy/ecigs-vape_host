be7a8107583beef1b5e0f54106b4a4877f2c5a2a
Oleh <salitrunskyy@gmail.com>
2023-08-07 18:16:04 +0300

    password validation

--- src/apps/account/api/serializers.py
+++ src/apps/account/api/serializers.py
@@ -41,6 +41,13 @@ class UserSerializer(serializers.ModelSerializer):
         if 'email' in data:
             if User.objects.filter(email=data['email']).exists():
                 raise serializers.ValidationError({'email': _(u'Email already registered')})
+        
+        if 'password' in data:
+            password = data['password']
+            if not re.match(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$", password):
+                raise serializers.ValidationError({
+                    'password': _(u'Password must be 8-16 characters with at least one letter and one number')
+                    })
 
         return data
     
