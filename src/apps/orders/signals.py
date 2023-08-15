d84f367964cd7f9d3f689b519e1d4067b7fb8d78
Oleh <salitrunskyy@gmail.com>
2023-08-15 11:26:33 +0300

    quick_save

--- src/apps/orders/signals.py
+++ src/apps/orders/signals.py
@@ -3,7 +3,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import OrderGoods
+from .models import Order, OrderGoods
+
+from .tasks import task_new_order_sendmail
 
 
 import logging
@@ -15,3 +17,10 @@ log = logging.getLogger(__name__)
 def signal_update_stock_count_post_save(sender, instance, created, **kwargs):
     pass
     # TODO
+
+
+@receiver([post_save,], dispatch_uid="signal_new_order_sendmail_post_save", sender=Order)
+def signal_new_order_sendmail_post_save(sender, instance, created, **kwargs):
+    if created:
+        task_new_order_sendmail(instance)
+        log.debug('New order recieved')
---
+++ src/apps/orders/tasks.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+def task_new_order_sendmail(order):
+    ...
