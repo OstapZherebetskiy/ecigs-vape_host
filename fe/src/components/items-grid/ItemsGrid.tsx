951de530f4f05ad88550a80e317875a40f1a717f
Taras Zherebetskiy <tarasj76@gmail.com>
2023-09-28 13:17:39 +0300

    Fixed card imgs

--- fe/src/components/items-grid/ItemsGrid.tsx
+++ fe/src/components/items-grid/ItemsGrid.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react'
 import { Product } from '@/api/product-microservice/types'
 import { productsApi } from '@/api/product-microservice'
+import { Button } from '@/common-ui/button'
 
 import style from './ItemsGrid.module.scss'
-import { Button } from '@/common-ui/button'
 
 export const ItemsGrid = () => {
   const [productList, setProductList] = useState<Product[]>([])
@@ -18,7 +18,7 @@ export const ItemsGrid = () => {
     setProductList(data)
   }
 
-  const getFormatedDesc = (desc: string) => {
+  const getFormattedDesc = (desc: string) => {
     if (desc.length <= 130) return desc
 
     const newDesc = desc.slice(0, 130).split(' ')
@@ -41,7 +41,7 @@ export const ItemsGrid = () => {
           ) : (
             <span className={style.absent}>Товар відсутній</span>
           )}
-          <p className={style.desc}>{getFormatedDesc(product.description)}</p>
+          <p className={style.desc}>{getFormattedDesc(product.description)}</p>
           <span className={style.price}>{product.price} грн</span>
           <Button className={style.btn}>Купити</Button>
         </li>
